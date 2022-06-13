8250fa370c727c4a47c66df4c53105f74ea8f13c
tugrulcansollu <tugrul.sollu@marleyspoon.com>
2022-06-13 21:35:15 +0200

    Fix documentation :green_book:

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,12 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-from havhav import get_dog_facts  # noqa
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("./../"))
+
 
 # -- Project information -----------------------------------------------------
 
